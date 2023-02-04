8be537e98d4a010577f042287dbff5560be73601
ashmalzahra <ashmal.zahraa@gmail.com>
2023-02-04 06:46:04 +0500

    Add new id and update file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
 import './style.css';
-import ScoreBoard from './modules/scores';
-
-const Leaderboard = new ScoreBoard
-
-window.onload = () => {
-    document.getElementById('submit').addEventListener('click', (e) => {
-        e.preventDefault()
-        Leaderboard.addScore()
-    })
-
-    document.getElementById('refresh').addEventListener('click', () => {
-        Leaderboard.refresh()
-    })
-}
--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,56 +1,21 @@
-const { createHtmlTagObject } = require("html-webpack-plugin");
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/y3PbjeUWfIHeWdOysNrq/scores/';
 
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/3mWwBXtnhJMzV9vmFTy1/scores/';
+export const createScore = async (user, score) => {
+  const API = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({ user, score }),
+  });
+  const data = await API.json();
+  const stat = API.status;
+  return { data, stat };
+};
 
-const submit = async (user, score) => {
-    await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(
-            { 
-                user: `${user}`,
-                score: `${score}`
-            }
-        )
-    })
-}
-
-export default class ScoreBoard {
-    constructor(){
-        this.list = []
-    }
-
-    addScore(){
-        const scoresObject = {}
-         scoresObject.user = document.getElementById('name').value
-         scoresObject.score = document.getElementById('score').value
-        if(scoresObject.user != '' && scoresObject.score != ''){
-            this.list.push(scoresObject)
-            submit(scoresObject.user, scoresObject.score)
-        }
-        createHtml()
-        document.getElementById('name').value = null
-        document.getElementById('score').value = null
-
-    }
-
-    createHtml(){
-        scores = document.querySelector('.scores')
-        scores.innerHtml = ''
-        this.list.forEach((scoresObject) => {
-            const element = document.createElement('li')
-            element.classList.add('each-score')
-            element.innerText = `${scoresObject.user}: ${scoresObject.score}`
-            scores.appendChild(element)
-        })
-    }
-
-    async refresh () {
-        this.response = await fetch(url)
-        .then((response) => response.json())
-        .then(this.list = this.response.result)
-        this.createHtml
-    }
-}   
+export const getScores = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const stat = response.status;
+    return { data, stat };
+  };
