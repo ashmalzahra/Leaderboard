f22bc1d59686e277ccf24dd2d27eae0ecf67a213
ashmalzahra <ashmal.zahraa@gmail.com>
2023-02-04 06:48:52 +0500

    Update index file for functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+import { refresh, addScore } from './modules/createScore';
+
+const refreshBtn = document.querySelector('#refresh');
+const form = document.querySelector('#form');
+const name = document.getElementById('name');
+const score = document.getElementById('score');
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  const Name = name.value;
+  const Score = score.value;
+  addScore(Name, Score);
+  form.reset();
+};
+
+refreshBtn.onclick = () => refresh();
